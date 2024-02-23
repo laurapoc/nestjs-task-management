d051257b31e168c36c6a6ef2c73147934d4de0b2
Laura Pociuniene <laurapociuniene@gmail.com>
2024-02-23 17:47:26 +0200

    Create first GET and POST API's

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
+
+  @Get()
+  //   similar name 'getAllTasks()' (as in tasks.service and tasks.conroller ) is not mandatory
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  // FIRST IMPLEMENTATION WAY:
+
+  // createTask(@Body() body) {
+  //   console.log('body', body);
+  // }
+
+  // SECOND IMPLEMENTATION WAY:
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    return this.tasksService.createTask(title, description);
+  }
 }
