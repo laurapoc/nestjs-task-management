843f6df68e11e551ef8422454786544d72f18dd0
Laura Pociuniene <laurapociuniene@gmail.com>
2024-03-08 16:50:01 +0200

    handle errors with pipes

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -38,7 +38,19 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    // try to get task
+
+    // if not found, throw an error (404 not found)
+
+    // otherwise, return the found task
+
+    const found = this.tasks.find((task) => task.id === id);
+
+    if (!found) {
+      // if no message is passed as a parameter, then only 404 not found is returned from response
+      throw new NotFoundException(`Task with ${id} not found`);
+    }
+    return found;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -57,7 +69,8 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const found = this.getTaskById(id)
+    this.tasks = this.tasks.filter((task) => task.id !== found.id);
   }
 
   updateTaskStatus(id: string, status: TaskStatus) {
