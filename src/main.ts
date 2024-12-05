270576104e960b1007f24cff2a3e0c1618015c03
Laura Pociuniene <laurapociuniene@gmail.com>
2024-12-05 15:21:48 +0200

    update packages, enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,10 @@ import { TransformInterceptor } from './transforms.interceptor';
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  const port = 3001;
+  const port = 3002;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
